bcff49c91cfb242c6463fdaa6b1331640c94cbb0
sWaRtHi <yadujain@gmail.com>
2019-05-23 16:35:54 +0530

    dropdown

--- src/components/HeaderLinks/index.jsx
+++ src/components/HeaderLinks/index.jsx
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import React from "react";
 // react components for routing our app without refresh
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -10,7 +10,7 @@ import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
 
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
+import { Apps } from "@material-ui/icons";
 
 // core components
 // import CustomDropdown from "../../components/CustomDropdown/CustomDropdown.jsx";
@@ -18,12 +18,37 @@ import Button from "../../components/Button";
 
 import headerLinksStyle from './styles';
 import Icon from '@material-ui/core/Icon';
+import Dropdown from "../Dropdown";
+
 
 function HeaderLinks({ ...props }) {
 	const { classes } = props;
 	console.log('classes', classes.signInIcons);
   return (
     <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Dropdown
+          noLiPadding
+          buttonText="Components"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              All components
+            </Link>,
+            <a
+              href=""
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>
+          ]}
+        />
+      </ListItem>
       <ListItem className={classes.listItem}>
         <Tooltip
           id="signin-tooltip"
